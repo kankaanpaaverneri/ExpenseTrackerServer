53e1b0ed6f023a840c37e670fa6c3e483faf08c1
Verneri Kankaanpää <verneri.kankaanpaa@gmail.com>
2024-12-02 20:32:49 +0200

    Add mysql and dotenv.

--- src/controller/getCategoriesController.ts
+++ src/controller/getCategoriesController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
 import { categories } from "../model/data";
+import sql from "../database/database";
 
-const getCategoriesController = (req: Request, res: Response) => {
+const getCategoriesController = async (req: Request, res: Response) => {
   console.log("Hello from getCategoriesController: ", req.body);
+  const response = await sql.execute("SHOW TABLES");
+  console.log("response: ", response);
   res.status(500).json({ categories: categories });
 };
 
